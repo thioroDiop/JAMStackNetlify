3608ddcc104a4e5e970a0ffb2b4481775d19565a
Thioro <dthioro@yahoo.fr>
2021-09-29 14:12:00 +0200

    Ajout du fichier admin et style

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
     // On copie le css directement vers la sortie
     eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addPassthroughCopy("admin/config.yml");
 };
